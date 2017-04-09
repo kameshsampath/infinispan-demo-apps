d35463eef1c43043e81c82934afedc6ee4b087d5
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-04-09 09:51:49 +0530

    First Import - with all roles working and checked except for EXEC

--- jdg-security-demo/src/test/java/org/workspace7/jdg/JDGSecurityTest.java
+++ jdg-security-demo/src/test/java/org/workspace7/jdg/JDGSecurityTest.java
@@ -4,9 +4,16 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.Configuration;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.fail;
@@ -22,8 +29,22 @@ public class JDGSecurityTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    public static final Properties jdgProperties = new Properties();
+
+    @BeforeClass
+    public static void loadProperties() {
+        try {
+            jdgProperties.load(JDGSecurityTest.class.getResourceAsStream("/jdg.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void testWriteOnlyRole() {
+        assertThat(jdgProperties).isNotNull();
+        assertThat(jdgProperties).isNotEmpty();
+        assertThat(jdgProperties).containsKey("infinispan.client.hotrod.server_list");
         Configuration configuration = new ConfigurationBuilder()
                 .connectionPool()
                 .maxTotal(1)
@@ -34,7 +55,7 @@ public class JDGSecurityTest {
                 .callbackHandler(new MyCallbackHandler("david",
                         SECURITY_REALM, "password".toCharArray()))
                 .saslMechanism("PLAIN")
-                .addServers("192.168.56.1:11722")
+                .addServers(jdgProperties.getProperty("infinispan.client.hotrod.server_list"))
                 .build();
         RemoteCacheManager remoteCacheManager = new RemoteCacheManager(configuration);
         testCache = remoteCacheManager.getCache("users-cache");
@@ -55,11 +76,11 @@ public class JDGSecurityTest {
                 .security()
                 .authentication()
                 .enable()
-                .serverName("192.168.56.1")
+                .serverName("node1-hotrod-server")
                 .callbackHandler(new MyCallbackHandler("raja",
                         SECURITY_REALM, "password".toCharArray()))
                 .saslMechanism("PLAIN")
-                .addServers("192.168.56.1:11722")
+                .addServers(jdgProperties.getProperty("infinispan.client.hotrod.server_list"))
                 .build();
         RemoteCacheManager remoteCacheManager = new RemoteCacheManager(configuration);
         testCache = remoteCacheManager.getCache("users-cache");
@@ -73,23 +94,56 @@ public class JDGSecurityTest {
     }
 
     @Test
-    public void testSupervisorRole() {
+    public void testManagersRole() {
         Configuration configuration = new ConfigurationBuilder()
                 .connectionPool()
                 .maxTotal(1)
                 .security()
                 .authentication()
                 .enable()
-                .serverName("192.168.56.1")
-                .callbackHandler(new MyCallbackHandler("admin",
+                .serverName("node1-hotrod-server")
+                .callbackHandler(new MyCallbackHandler("james",
                         SECURITY_REALM, "password".toCharArray()))
                 .saslMechanism("PLAIN")
-                .addServers("192.168.56.1:11722")
+                .addServers(jdgProperties.getProperty("infinispan.client.hotrod.server_list"))
                 .build();
         RemoteCacheManager remoteCacheManager = new RemoteCacheManager(configuration);
         testCache = remoteCacheManager.getCache("users-cache");
         testCache.put("testKey3", "value3");
         assertThat(testCache.get("testKey3")).isNotNull();
         assertThat(testCache.get("testKey3")).isEqualTo("value3");
+    }
+
+    //ADD test to check admin role
+
+    @Test
+    public void testClusterAdminsRole() {
+        Map<String,String> values = new HashMap<>();
+        values.put("key25","25");
+        values.put("key26","26");
+        values.put("key27","27");
+        Configuration configuration = new ConfigurationBuilder()
+                .connectionPool()
+                .maxTotal(1)
+                .security()
+                .authentication()
+                .enable()
+                .serverName("node1-hotrod-server")
+                .callbackHandler(new MyCallbackHandler("admin",
+                        SECURITY_REALM, "password".toCharArray()))
+                .saslMechanism("PLAIN")
+                .addServers(jdgProperties.getProperty("infinispan.client.hotrod.server_list"))
+                .build();
+        RemoteCacheManager remoteCacheManager = new RemoteCacheManager(configuration);
+        testCache = remoteCacheManager.getCache("users-cache");
+        testCache.put("testKey3", "value3"); //single-put operation
+        assertThat(testCache.get("testKey3")).isNotNull();
+        assertThat(testCache.get("testKey3")).isEqualTo("value3");
+        testCache.putAll(values); // bulk write
+        Set<String> keys = testCache.keySet(); //bulk read
+        assertThat(keys).contains("key25","key26","key27");
+        testCache.clear(); // bulk write again
+
+        //TODO exec
     }
 }
